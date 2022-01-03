3d2031cb9c7e1c2d221e7f227c619f4fcc18c0ca
Chris Boyle <cthboyle6@gmail.com>
2022-01-03 16:32:33 -0500

    initialize and connected ExpenseFilter component to ExpenseContainer

--- src/components/Expenses/ExpenseContainer.js
+++ src/components/Expenses/ExpenseContainer.js
@@ -1,20 +1,33 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 
 import "./ExpenseContainer.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 
-const ExpenseContainer = ({expenses}) => {
+const ExpenseContainer = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2021");
+
+  const selectedYearHandler = (selected) => {
+    setSelectedYear(selected);
+  };
+
   return (
-    <div className="expenses">
-      {expenses.map(expense => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            date={expense.date}
-            amount={expense.amount}
-          />
-        );
-      })}
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selectedYear={selectedYear} onSelectYear={selectedYearHandler} />
+        {props.expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              date={expense.date}
+              amount={expense.amount}
+            />
+          );
+        })}
+      </Card>
     </div>
   );
 };
